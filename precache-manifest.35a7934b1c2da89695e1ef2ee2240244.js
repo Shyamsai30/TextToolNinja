c5ee7ff696c055eccb500bed666e24eedf9fcb11
Shyam Sai Ganesh <saiganesh4551@gmail.com>
2023-06-05 14:56:30 +0530

    Updates

---
+++ precache-manifest.35a7934b1c2da89695e1ef2ee2240244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bc4c77caa60ce763526",
+    "url": "/TextToolNinja/static/css/main.04463b96.chunk.css"
+  },
+  {
+    "revision": "7bc4c77caa60ce763526",
+    "url": "/TextToolNinja/static/js/main.7bc4c77c.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextToolNinja/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextToolNinja/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "7e085d7c3893b7935121",
+    "url": "/TextToolNinja/static/js/runtime~main.7e085d7c.js"
+  },
+  {
+    "revision": "a3eda096073f74d7b57dbc91f344c639",
+    "url": "/TextToolNinja/index.html"
+  }
+];
